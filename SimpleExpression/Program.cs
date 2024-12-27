6436d6dcd9c13a0a5d2cc6eac3134d3c03dfff13
ext1z <ext1z1tabgod@gmail.com>
2024-12-27 14:07:32 +0500

    changed

--- SimpleExpression/Program.cs
+++ SimpleExpression/Program.cs
@@ -5,9 +5,12 @@ class Program
 {
     static void Main()
     {
-        int a = 1, b = 2, c = 3;
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
         int minValue = FindMinimum(a, b, c);
-        Console.WriteLine($"Min value: {minValue}");
+        Console.WriteLine(minValue);
     }
 
     static int FindMinimum(int a, int b, int c)
@@ -20,30 +23,40 @@ class Program
         {
             foreach (var op2 in operators)
             {
-                int result = Calculate(a,b,c,op1,op2);
+                int result = Calculate(a, b, c, op1, op2);
                 minValue = Math.Min(minValue, result);
             }
-            
+
             // Local function
             static int Calculate(int num1, int num2, int num3, string operator1, string operator2)
             {
                 int temp;
-                if(operator1 == "*")
+
+                if (operator2 == "*")
+                {
+                    temp = num2 * num3;
+                    return operator1 == "+" ? num1 + temp : num1 - temp;
+                }
+                else if (operator1 == "*" && operator2 != "*")
+                {
                     temp = num1 * num2;
-                else if(operator1 == "+")
-                    temp = num1 + num2;
+                    return operator2 == "+" 
+                        ? temp + num3 
+                        : temp - num3;
+                }
                 else
-                    temp = num1 - num2;
-
-                if(operator2 == "*")
-                    return temp * num3;
-
-                else if (operator2 == "+")
-                    return temp + num3;
-                else
-                    return temp - num3;
+                {
+                    temp = operator1 == "+" 
+                        ? num1 + num2 
+                        : num1 - num2;
+                    return operator2 == "+" 
+                        ? temp + num3 
+                        : temp - num3;
+                }
             }
         }
         return minValue;
     }
 }
+
+
